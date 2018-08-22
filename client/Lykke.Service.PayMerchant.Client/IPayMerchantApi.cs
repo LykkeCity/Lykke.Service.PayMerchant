dc80787e519f8390de7de4c7ba5b9df4838b539c
tarurar <tarutin_andrew@mail.ru>
2018-08-22 13:42:33 +0300

    fixed automapper registration, added client lib, covered crud operations for merchants controller

--- client/Lykke.Service.PayMerchant.Client/IPayMerchantApi.cs
+++ client/Lykke.Service.PayMerchant.Client/IPayMerchantApi.cs
@@ -1,4 +1,7 @@
-﻿using JetBrains.Annotations;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Lykke.Service.PayMerchant.Client.Models;
 using Refit;
 
 namespace Lykke.Service.PayMerchant.Client
@@ -9,5 +12,53 @@ namespace Lykke.Service.PayMerchant.Client
     [PublicAPI]
     public interface IPayMerchantApi
     {
+        /// <summary>
+        /// Get all merchants
+        /// </summary>
+        /// <returns></returns>
+        [Get("/api/merchants")]
+        Task<IReadOnlyList<MerchantModel>> GetAllAsync();
+
+        /// <summary>
+        /// Get merchant by id
+        /// </summary>
+        /// <param name="merchantId"></param>
+        /// <returns></returns>
+        [Get("/api/merchants/{merchantId}")]
+        Task<MerchantModel> GetByIdAsync(string merchantId);
+
+        /// <summary>
+        /// Creates merchant
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [Post("/api/merchants")]
+        Task<MerchantModel> CreateAsync([Body] CreateMerchantRequest request);
+
+        /// <summary>
+        /// Updates merchant
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [Patch("/api/merchants")]
+        Task UpdateAsync([Body] UpdateMerchantRequest request);
+
+        /// <summary>
+        /// Updates merchant's public key
+        /// </summary>
+        /// <param name="merchantId"></param>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        [Multipart]
+        [Post("/api/merchants/{merchantId}/publickey")]
+        Task SetPublicKeyAsync(string merchantId, [AliasAs("file")] StreamPart stream);
+
+        /// <summary>
+        /// Deletes merchant by id
+        /// </summary>
+        /// <param name="merchantId"></param>
+        /// <returns></returns>
+        [Delete("/api/merchants/{merchantId}")]
+        Task DeleteAsync(string merchantId);
     }
 }
