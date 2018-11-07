328f3781f2807083eceec6a7837d01c8645194d3
Vitalii Dasaev <vitalii.dasaev@lykke.com>
2018-11-07 18:39:55 +0300

    LPDEV-936 remove whitespaces on merchant creation and edit

---
+++ src/Lykke.Service.PayMerchant.Core/Extensions/MerchantExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lykke.Service.PayMerchant.Core.Domain;
+
+namespace Lykke.Service.PayMerchant.Core.Extensions
+{
+    public static class MerchantExtensions
+    {
+        public static void TrimProperties(this IMerchant merchant)
+        {
+            merchant.Name = merchant.Name.Trim();
+            merchant.DisplayName = merchant.DisplayName.Trim();
+            merchant.Email = merchant.Email.Trim();
+        }
+    }
+}
